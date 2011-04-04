2f6558caf4252f314dccfa85daa3a093cb3f7985
Kimmo Varis <ext-kimmo.1.varis@nokia.com>
2011-04-04 15:38:47 +0300

    Tests: Add test acquiring for two resource sets.

---
+++ tests/test-acquire/test-acquire.cpp
@@ -0,0 +1,150 @@
+/*************************************************************************
+This file is part of libresourceqt
+
+Copyright (C) 2010 Nokia Corporation.
+
+This library is free software; you can redistribute
+it and/or modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation
+version 2.1 of the License.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+USA.
+*************************************************************************/
+
+#include <QSignalSpy>
+#include <QList>
+#include <QEventLoop>
+#include <QTimer>
+#include "test-acquire.h"
+
+using namespace ResourcePolicy;
+
+Resource * TestAcquire::resourceFromType(ResourceType type)
+{
+    switch (type) {
+    case AudioPlaybackType:
+        return new AudioResource;
+    case AudioRecorderType:
+        return new AudioRecorderResource;
+    case VideoPlaybackType:
+        return new VideoResource;
+    case VideoRecorderType:
+        return new VideoRecorderResource;
+    case VibraType:
+        return new VibraResource;
+    case LedsType:
+        return new LedsResource;
+    case BacklightType:
+        return new BacklightResource;
+    case SystemButtonType:
+        return new SystemButtonResource;
+    case LockButtonType:
+        return new LockButtonResource;
+    case ScaleButtonType:
+        return new ScaleButtonResource;
+    case SnapButtonType:
+        return new SnapButtonResource;
+    case LensCoverType:
+        return new LensCoverResource;
+    case HeadsetButtonsType:
+        return new HeadsetButtonsResource;
+    default:
+        return NULL;
+    }
+}
+
+using namespace ResourcePolicy;
+
+TestAcquire::TestAcquire()
+    : audioResource(NULL)
+    , audioRecorderResource(NULL)
+    , videoResource(NULL)
+    , videoRecorderResource(NULL)
+    , vibraResource(NULL)
+    , ledsResource(NULL)
+    , backlightResource(NULL)
+    , systemButtonResource(NULL)
+    , lockButtonResource(NULL)
+    , scaleButtonResource(NULL)
+    , snapButtonResource(NULL)
+    , lensCoverResource(NULL)
+    , headsetButtonsResource(NULL)
+{
+}
+
+TestAcquire::~TestAcquire()
+{
+}
+
+void TestAcquire::waitForSignal(const QObject *sender, const char *signal, quint32 timeout)
+{
+    QEventLoop loop;
+    QTimer::singleShot(timeout, &loop, SLOT(quit()));
+    loop.connect(sender, signal, SLOT(quit()));
+    loop.exec();
+}
+
+void TestAcquire::testAcquire()
+{
+    ResourceSet resourceSet("player");
+    ResourceSet resourceSet2("player");
+
+    // Test that signals gets emitted
+    QSignalSpy stateSpyGranted(&resourceSet,
+            SIGNAL(resourcesGranted(const QList<ResourcePolicy::ResourceType> &)));
+    QVERIFY(stateSpyGranted.isValid());
+    QSignalSpy stateSpyReleased(&resourceSet, SIGNAL(resourcesReleased()));
+    QVERIFY(stateSpyReleased.isValid());
+    QSignalSpy stateSpyGranted2(&resourceSet2,
+            SIGNAL(resourcesGranted(const QList<ResourcePolicy::ResourceType> &)));
+    QVERIFY(stateSpyGranted2.isValid());
+    QSignalSpy stateSpyReleased2(&resourceSet2, SIGNAL(resourcesReleased()));
+    QVERIFY(stateSpyReleased2.isValid());
+
+    bool addOk = resourceSet.addResource(AudioPlaybackType);
+    QVERIFY(addOk);
+    bool connectOk = resourceSet.initAndConnect();
+    QVERIFY(connectOk);
+    bool addOk2 = resourceSet2.addResource(AudioPlaybackType);
+    QVERIFY(addOk2);
+    bool connectOk2 = resourceSet2.initAndConnect();
+    QVERIFY(connectOk2);
+
+    bool acquireOk = resourceSet.acquire();
+    QVERIFY(acquireOk);
+    waitForSignal(&resourceSet, SIGNAL(resourcesGranted(const QList<ResourcePolicy::ResourceType> &)));
+
+    // Check the signal parameters
+    QCOMPARE(stateSpyGranted.count(), 1);
+
+    bool releaseOk = resourceSet.release();
+    QVERIFY(releaseOk);
+    waitForSignal(&resourceSet, SIGNAL(resourcesReleased()));
+
+    // Check the signal parameters
+    QCOMPARE(stateSpyReleased.count(), 1);
+
+    bool acquireOk2 = resourceSet2.acquire();
+    QVERIFY(acquireOk2);
+    waitForSignal(&resourceSet2, SIGNAL(resourcesGranted(const QList<ResourcePolicy::ResourceType> &)));
+
+    // Check the signal parameters
+    QCOMPARE(stateSpyGranted2.count(), 1);
+
+    bool releaseOk2 = resourceSet2.release();
+    QVERIFY(releaseOk2);
+    waitForSignal(&resourceSet2, SIGNAL(resourcesReleased()));
+
+    // Check the signal parameters
+    QCOMPARE(stateSpyReleased2.count(), 1);
+}
+
+QTEST_MAIN(TestAcquire)
