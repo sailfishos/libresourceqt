8071e248c6bf997904795f2b4b58d60a9b0c7f9e
Kimmo Varis <ext-kimmo.1.varis@nokia.com>
2011-05-19 12:25:59 +0300

    Tests: Another fix for player-player test.

--- tests/test-released-by-manager/test-released-by-manager.cpp
+++ tests/test-released-by-manager/test-released-by-manager.cpp
@@ -1194,12 +1194,9 @@ void TestReleasedByManager::testLostAutoReleaseBothPlayer()
     // Acquire the resource for the second client
     bool acquireOk2 = resourceSet2.acquire();
     QVERIFY(acquireOk2);
-    // Wait for the lost-signal for the first client
-    // The lost-signal for the first client must become before the granted-
-    // signal for the second client
-    waitForSignal(&resourceSet, SIGNAL(lostResources()));
-    //QCOMPARE(stateSpyGranted2.count(), 0);
-    QCOMPARE(stateSpyLost.count(), 1);
+    // Wait for the manager released-signal for the first client
+    waitForSignal(&resourceSet, SIGNAL(resourcesReleasedByManager()));
+    QCOMPARE(stateSpyResourcesReleased.count(), 1);
     // Wait for the granted-signal for the second client
     waitForSignal(&resourceSet2, SIGNAL(resourcesGranted(const QList<ResourcePolicy::ResourceType> &)));
     QCOMPARE(stateSpyGranted2.count(), 1);
@@ -1213,11 +1210,11 @@ void TestReleasedByManager::testLostAutoReleaseBothPlayer()
 
     // Check all the signals
     QCOMPARE(stateSpyGranted.count(), 1);
-    QCOMPARE(stateSpyLost.count(), 1);
+    QCOMPARE(stateSpyLost.count(), 0);
     QCOMPARE(stateSpyReleased.count(), 0);
     QCOMPARE(stateSpyDenied.count(), 0);
     QCOMPARE(stateSpyUpdateOK.count(), 0);
-    QCOMPARE(stateSpyResourcesReleased.count(), 0);
+    QCOMPARE(stateSpyResourcesReleased.count(), 1);
     QCOMPARE(stateSpyBecameAvailable.count(), 1);
 
     QCOMPARE(stateSpyGranted2.count(), 1);
